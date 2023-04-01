69e1336b44f0b4d37d1675a0178b0c8ee72d4784
tic <kylecoxforweb@gmail.com>
2023-04-01 08:55:32 +0100

    added basic text element

--- MasterfulUI/src/main/java/io/ticticboom/mods/mui/Ref.java
+++ MasterfulUI/src/main/java/io/ticticboom/mods/mui/Ref.java
@@ -11,5 +11,6 @@ public class Ref {
     public static final class DocTypes {
         public static final ResourceLocation SCREENS = new ResourceLocation(ID, "screen");
         public static final ResourceLocation IMAGE_ELEMENT = new ResourceLocation(ID, "element/image");
+        public static final ResourceLocation TEXT_ELEMENT = new ResourceLocation(ID, "element/text");
     }
 }
---
+++ MasterfulUI/src/main/java/io/ticticboom/mods/mui/screen/element/TextElement.java
@@ -0,0 +1,65 @@
+package io.ticticboom.mods.mui.screen.element;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import io.ticticboom.mods.mconf.document.DocumentValidationError;
+import io.ticticboom.mods.mconf.document.IConfigSpecConsumer;
+import io.ticticboom.mods.mconf.document.ThrowingConfigSpecConsumer;
+import io.ticticboom.mods.mconf.parser.IParseableDocument;
+import io.ticticboom.mods.mconf.parser.IParseableDocumentSpec;
+import io.ticticboom.mods.mconf.setup.document.ConfigDocumentType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.List;
+
+public class TextElement extends ConfigDocumentType {
+    @Override
+    public IConfigSpecConsumer createSpecConsumer() {
+        return new SpecConsumer();
+    }
+
+    public static final class SpecConsumer extends ThrowingConfigSpecConsumer<Spec> {
+        @Override
+        public Spec safeParse(IParseableDocumentSpec doc) {
+            var text = doc.get("text").getString();
+            var x = doc.get("x").getInteger();
+            var y = doc.get("y").getInteger();
+            return new Spec(text, x, y);
+        }
+
+        @Override
+        public boolean safeValidate(Spec spec, List<DocumentValidationError> list) {
+            return true;
+        }
+
+        @Override
+        public void safeConsume(Spec spec, IParseableDocument doc) {
+            ScreenElements.REGISTRY.put(doc.getId(), spec);
+            ScreenElements.HANDLERS.put(doc.getId(), new SpecHandler(spec));
+        }
+    }
+
+    public static final class SpecHandler extends ScreenElements.SpecHandler {
+
+        private final Spec spec;
+
+        public SpecHandler(Spec spec) {
+
+            this.spec = spec;
+        }
+
+        @Override
+        public void render(Screen screen, PoseStack pose, int mouseX, int mouseY, float partialTicks) {
+            Minecraft.getInstance().font.draw(pose, spec.text, spec.x, spec.y, 0x474747);
+        }
+    }
+
+    public record Spec(
+            String text,
+            int x,
+            int y
+    ) implements ScreenElements.IElementSpec {
+
+    }
+}
